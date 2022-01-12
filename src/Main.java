07a407612cf3d98ddbf512fbb4854dde0d320b5d
Denis Podkovyrov <denisvyrov@yandex.ru>
2022-01-12 15:04:27 +0500

    dev 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "C:/Users/Denis/IdeaProjects";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        long duration = (System.currentTimeMillis() - start);
+        System.out.println(duration + " ms");
     }
 
     public static long getFolderSize(File folder) {
